5772c1dcefafe7726d359583dbe8be324435364a
George Kalpakas <kalpakas.g@gmail.com>
2018-05-29 13:50:39 +0300

    fix: clean up AngularJS app on destruction

--- src/app/route-4/route-4.component.ts
+++ src/app/route-4/route-4.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
 import * as angular from 'angular';
+import { Subject } from 'rxjs';
 import { TextUtilsService } from '../shared/text-utils.service';
 import { UpgradeUtilsService } from '../upgrade-utils/upgrade-utils.service';
 import { moduleName } from './route-4-angularjs.module';
@@ -10,15 +11,22 @@ import { moduleName } from './route-4-angularjs.module';
     <div route-4-angularjs-root></div>
   `,
 })
-export class Route4Component implements AfterViewInit {
+export class Route4Component implements AfterViewInit, OnDestroy {
   title = 'Route 4';
+  private onDestroy$ = new Subject<void>();
 
   constructor(private elementRef: ElementRef, private upgradeUtils: UpgradeUtilsService) { }
 
   ngAfterViewInit() {
     const angularjsRoot = this.elementRef.nativeElement.querySelector('[route-4-angularjs-root]');
-    this.upgradeUtils.bootstrap(angularjsRoot, moduleName, {
+    const cleanUp = this.upgradeUtils.bootstrap(angularjsRoot, moduleName, {
       textUtils: TextUtilsService,
     });
+
+    this.onDestroy$.subscribe(cleanUp);
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
   }
 }
--- src/app/upgrade-utils/upgrade-utils.service.ts
+++ src/app/upgrade-utils/upgrade-utils.service.ts
@@ -12,14 +12,18 @@ export interface DowngradableProviders {
 export class UpgradeUtilsService {
   constructor(private injector: Injector, private ngZone: NgZone) { }
 
-  bootstrap(root: HTMLElement, moduleName: string, providers: DowngradableProviders): void {
+  bootstrap(root: HTMLElement, moduleName: string, providers: DowngradableProviders): () => void {
     const downgradedProviders = [
       '$provide',
       ($provide: angular.auto.IProvideService) =>
         Object.keys(providers).forEach(key =>
           $provide.factory(key, () => this.injector.get(providers[key]))),
     ];
+    let injector: angular.auto.IInjectorService;
 
-    this.ngZone.runOutsideAngular(() => angular.bootstrap(root, [moduleName, downgradedProviders]));
+    this.ngZone.runOutsideAngular(() =>
+      injector = angular.bootstrap(root, [moduleName, downgradedProviders], {strictDi: true}));
+
+    return () => injector.get('$rootScope').$destroy();
   }
 }
